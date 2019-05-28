8110a3b701536283e30b7be306255b1eb76f46c0
luogen <luogen@gmail.com>
2019-05-28 22:24:39 +0800

    用户信息处理提交

---
+++ app/src/main/java/com/example/win10/personality_newsapp/user/LoginedActivity.java
@@ -0,0 +1,56 @@
+package com.example.win10.personality_newsapp.user;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.win10.personality_newsapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginedActivity extends AppCompatActivity {
+
+    private List<Setting> settingList=new ArrayList<>();
+    private DividerItemDecoration mDividerItemDecoration;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_user_view);
+
+        initSetting();  //初始化用户设置列表
+        RecyclerView recyclerView =(RecyclerView) findViewById(R.id.recycler_user) ;
+        LinearLayoutManager layoutManager =new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false){
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
+        mDividerItemDecoration=new DividerItemDecoration(recyclerView.getContext(),layoutManager.getOrientation());
+        recyclerView.addItemDecoration(mDividerItemDecoration);
+        recyclerView.setLayoutManager(layoutManager);
+        SettingAdapter adapter =new SettingAdapter(settingList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initSetting(){
+        for(int i=0;i<1;i++){
+            /*Setting list1 = new Setting("我的关注",R.drawable.next);
+            settingList.add(list1);
+            Setting list2 = new Setting("我的钱包",R.drawable.next);
+            settingList.add(list2);
+            Setting list3= new Setting("消息通知",R.drawable.next);
+            settingList.add(list3);
+            Setting list4 = new Setting("扫一扫",R.drawable.next);
+            settingList.add(list4);
+            Setting list5 = new Setting("阅读公益",R.drawable.next);
+            settingList.add(list5);
+            Setting list6 = new Setting("用户反馈",R.drawable.next);
+            settingList.add(list6);*/
+            Setting list7= new Setting("系统设置",R.drawable.next);
+            settingList.add(list7);
+        }
+    }
+}
